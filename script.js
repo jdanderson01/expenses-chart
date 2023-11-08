dcc5739b53ca4d0186952a8c7feb7239f909b57a
toiletsplurge <MrTakeTooMuch@gmail.com>
2023-11-08 01:54:19 -0500

    Bar chart logic
    Added code to get the current date in its abbreviated form then added code to check and see what the current day was. Once the current day was calculated the color of the corresponding bar would change

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const chartCanvas = document.getElementById("myChart");
 
+const date = new Date();
+const options = { weekday: "short" };
+const abbreviatedDay = date.toLocaleDateString("en-US", options);
+
 //Get JSON data
 fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
     const labels = data.map((item) => item.day);
     const amounts = data.map((item) => item.amount);
+
+    const backgroundColors = labels.map((day) =>
+      day === abbreviatedDay.toLowerCase()
+        ? "hsl(186, 34%, 60%)"
+        : "hsl(10, 79%, 65%)"
+    );
 
     new Chart(chartCanvas, {
       type: "bar",
@@ -15,7 +25,7 @@ fetch("data.json")
           {
             label: "Amount",
             data: amounts,
-            backgroundColor: "hsl(10, 79%, 65%)",
+            backgroundColor: backgroundColors,
             borderWidth: 1,
             borderRadius: 5,
             bordrSkipped: false,
